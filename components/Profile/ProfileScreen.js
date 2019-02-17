3f41b07685381cd5699a6c322bed97ccaa495c9c
huynet <cocwedc@gmail.com>
2019-02-17 14:43:21 -0600

    spacing

--- components/Profile/ProfileScreen.js
+++ components/Profile/ProfileScreen.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { 
-     StyleSheet, 
-     Text, 
-     View, Image, 
-     ScrollView, 
-     TouchableOpacity,
-     SegmentedControlIOS,
-     SafeAreaView
+import {
+	StyleSheet,
+	Text,
+	View, Image,
+	ScrollView,
+	TouchableOpacity,
+	SegmentedControlIOS,
+	SafeAreaView
 } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -15,119 +15,120 @@ import OrderHistory from './OrderHistory'
 
 class ClassesScreen extends Component {
 
-     static navigationOptions = ({ navigation }) => {
-          return {
-               title: 'Profile',
-               headerRight: (
-                    <TouchableOpacity
-                         style={{paddingRight: 10}}
-                         onPress={() => {
-                              navigation.navigate('Settings')
-                         }}
-                    >
-                         <Ionicons name="ios-options" color='#007AFF' size={28}/>
-                    </TouchableOpacity>
-               )
-          }
-     }
+	static navigationOptions = ({ navigation }) => {
+		return {
+			title: 'Profile',
+			headerRight: (
+				<TouchableOpacity
+					style={{ paddingRight: 10 }}
+					onPress={() => {
+						navigation.navigate('Settings')
+					}}
+				>
+					<Ionicons name="ios-options" color='#007AFF' size={28} />
+				</TouchableOpacity>
+			)
+		}
+	}
 
-     state = {
-          selectedIndex: 0
-     }
+	state = {
+		selectedIndex: 0
+	}
 
-     render() {
-          return (
-               <View style={styles.container}>
-                    <ScrollView 
-                         showsVerticalScrollIndicator={false}
-                    >
-                         <View style={styles.titleBar}>
-                         <Image style={styles.avatar} source={require('../../assets/huy.jpg')} />
-                         <Text style={styles.name}>Huy Pham</Text>   
-                         <Text style={styles.title}>@huypham50</Text>
-                         </View>
-                         
-                         {/* <Text style={styles.subTitle}>
+	render() {
+		return (
+			<View style={styles.container}>
+				<ScrollView
+					showsVerticalScrollIndicator={false}
+				>
+					<View style={styles.titleBar}>
+						<Image style={styles.avatar} source={require('../../assets/huy.jpg')} />
+						<Text style={styles.name}>Huy Pham</Text>
+						<Text style={styles.title}>@huypham50</Text>
+					</View>
+
+					{/* <Text style={styles.subTitle}>
                          Your classes
                          </Text> */}
 
-                         <SafeAreaView style={styles.segmentedIOS}>
-                              <SegmentedControlIOS
-                                   values={['Schedule', 'Order\'s History']}
-                                   selectedIndex={this.state.selectedIndex}
-                                   onChange={(event) => {
-                                        this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
-                                   }}
-                              />
-                              <View>
-                              {(this.state.selectedIndex === 0) ? (
-                                   <Text style={styles.text}>
-                                        Current index is 0
+					<SafeAreaView>
+						<SegmentedControlIOS
+							style={styles.segmentedIOS}
+							values={['Schedule', 'Order\'s History']}
+							selectedIndex={this.state.selectedIndex}
+							onChange={(event) => {
+								this.setState({ selectedIndex: event.nativeEvent.selectedSegmentIndex });
+							}}
+						/>
+						<View>
+							{(this.state.selectedIndex === 0) ? (
+								<Text style={styles.text}>
+									Current index is 0
                                    </Text>
-                                   ) : (
-                                   <OrderHistory />
-                              )}
-                              </View>
-                         </SafeAreaView>
-                    </ScrollView>
-               </View>
-          );
-     }
+							) : (
+									<OrderHistory />
+								)}
+						</View>
+					</SafeAreaView>
+				</ScrollView>
+			</View>
+		);
+	}
 }
 
 export default ClassesScreen;
 
 const styles = StyleSheet.create({
-     container: {
-          flex: 1,
-          backgroundColor: 'rgb(255, 255, 255)'
-     },
-     segmentedIOS: {
-          marginTop: 15,
-          marginLeft: 30,
-          marginRight: 30
-     },
-     avatar: {
-          width: 80,
-          height: 80,
-          backgroundColor: 'black',
-          borderRadius: 40,
-     },
-     titleBar: {
-          width: '100%',
-          marginTop: 15,
-          justifyContent: 'center',
-          alignItems: 'center',
-     },
-     title: {
-          fontSize: 16,
-          fontFamily: 'Avenir Next',
-          color: '#b8bece',
-          fontWeight: '500',
-     },
-     subTitle: {
-          fontSize: 15,
-          fontFamily: 'Avenir Next',
-          color: '#b8bece',
-          fontWeight: '600',
-          marginLeft: 30,
-          marginTop: 25,
-          textTransform: 'uppercase'
-     },
-     name: {
-          fontSize: 20,
-          fontFamily: 'Avenir Next',
-          color: '#3c4560',
-          fontWeight: '700',
-          paddingTop: 5
-     },
-     classes: {
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'space-evenly',
-     },
-     text: {
-          fontFamily: 'Avenir Next',
-          fontSize: 20,
-     }
+	container: {
+		flex: 1,
+		backgroundColor: 'rgb(255, 255, 255)'
+	},
+	segmentedIOS: {
+		marginTop: 15,
+		marginLeft: 30,
+		marginRight: 30
+	},
+	avatar: {
+		width: 80,
+		height: 80,
+		backgroundColor: 'black',
+		borderRadius: 40,
+	},
+	titleBar: {
+		width: '100%',
+		marginTop: 15,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		fontSize: 16,
+		fontFamily: 'Avenir Next',
+		color: '#b8bece',
+		fontWeight: '500',
+	},
+	subTitle: {
+		fontSize: 15,
+		fontFamily: 'Avenir Next',
+		color: '#b8bece',
+		fontWeight: '600',
+		marginLeft: 30,
+		marginTop: 25,
+		textTransform: 'uppercase'
+	},
+	name: {
+		fontSize: 20,
+		fontFamily: 'Avenir Next',
+		color: '#3c4560',
+		fontWeight: '700',
+		paddingTop: 5
+	},
+	classes: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-evenly',
+	},
+	text: {
+		fontFamily: 'Avenir Next',
+		fontSize: 20,
+	}
 });
